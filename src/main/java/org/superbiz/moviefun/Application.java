6a76dbe349836a7fe6fb10ba45a3b3694a1c83e9
nss_admin <ashwin.gudidevuni@gmail.com>
2018-06-19 15:37:31 -0500

    modified:   pom.xml 	new file:   src/.DS_Store 	modified:   src/main/java/org/superbiz/moviefun/ActionServlet.java 	new file:   src/main/java/org/superbiz/moviefun/AlbumsDatabaseConfig.java 	modified:   src/main/java/org/superbiz/moviefun/Application.java 	new file:   src/main/java/org/superbiz/moviefun/DatabaseConfig.java 	new file:   src/main/java/org/superbiz/moviefun/DatabaseServiceCredentials.java 	modified:   src/main/java/org/superbiz/moviefun/HomeController.java 	new file:   src/main/java/org/superbiz/moviefun/MoviesDatabaseConfig.java 	modified:   src/main/java/org/superbiz/moviefun/albums/AlbumsBean.java 	modified:   src/main/java/org/superbiz/moviefun/movies/MoviesBean.java 	modified:   src/main/resources/application.yml 	modified:   src/test/java/org/superbiz/moviefun/SmokeTest.java

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -1,11 +1,19 @@
 package org.superbiz.moviefun;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class Application {
 
     public static void main(String... args) {
